5be99e4a815600d98c2b3509a25388b257ed73ac
soraefir <soraefir+git@helcel>
2026-05-22 21:15:46 +0200

    fix deploy ci

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  // Automatically applies the repo name in GitHub Actions, but stays empty for local npm run dev
+  basePath: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : '',
   images: {
     unoptimized: true
   },
